9ec258d0ff4f9207e3217a996faf77f0794be290
IonutMarius <barsanionut62@yahoo.com>
2016-07-20 20:05:43 +0300

    changed test database to in-memory hsqldb

--- estore-parent/estore-model/src/test/java/ro/estore/model/config/JpaHibernateTestConfig.java
+++ estore-parent/estore-model/src/test/java/ro/estore/model/config/JpaHibernateTestConfig.java
@@ -1,12 +1,25 @@
 package ro.estore.model.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 @Configuration
 @Import(RootJpaHibernateConfig.class)
 @PropertySource("classpath:test.properties")
 public class JpaHibernateTestConfig {
 
+	@Bean
+	public DataSource dataSource() {
+		EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+		EmbeddedDatabase db = builder.setType(EmbeddedDatabaseType.HSQL).addScript("db/sql/create-db-hsql.sql")
+				.addScript("db/sql/insert-data-hsql.sql").setName("testdb").build();
+		return db;
+	}
 }
